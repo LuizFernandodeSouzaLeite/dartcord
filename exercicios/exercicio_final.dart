38e59363c3189fa1e375d88cc52f8ccd902d8769
Luiz Fernando de Souza Leite <167731825+LuizFernandodeSouzaLeite@users.noreply.github.com>
2026-09-25 18:05:51 -0400

    feat: corrige a main

---
+++ exercicios/exercicio_final.dart
@@ -0,0 +1,47 @@
+import '../models/usuario.dart';
+import '../models/moderador.dart';
+import '../models/mensagem.dart';
+import '../models/canal.dart';
+import '../models/conteudo.dart';
+
+void gerarRelatorio() {
+  final usuario = Usuario(nome: 'Pedro', apelido: 'PG', online: true);
+  final moderadora = Moderador(nome: 'Ana', apelido: 'Ana', online: true);
+  final usuarios = <Usuario>[usuario, moderadora];
+
+  final geral = Canal(nome: 'geral');
+  final canalDart = Canal(nome: 'dart');
+  final flutter = Canal(nome: 'flutter');
+  final canais = <Canal>[geral, canalDart, flutter];
+
+  canalDart.adicionarMensagem(
+    Mensagem(autor: usuario, texto: 'Olá, DartCord!'),
+  );
+  canalDart.adicionarMensagem(
+    Mensagem(autor: moderadora, texto: 'Continuem praticando.'),
+  );
+
+  print('\nDARTCORD');
+  print('Servidor: Programação Mobile');
+
+  print('Usuários online:');
+  for (final pessoa in usuarios) {
+    if (pessoa.online) {
+      print(pessoa.apelido);
+    }
+  }
+
+  print('Canais:');
+  for (final canal in canais) {
+    print('#${canal.nome}');
+  }
+
+  print('Exibindo mensagens do canal:');
+  print('#${canalDart.nome}');
+  for (final mensagem in canalDart.mensagens) {
+    print('${mensagem.autor.apelido}: ${mensagem.texto}');
+  }
+
+  Conteudo aviso = Aviso('Relatório gerado.');
+  aviso.exibir();
+}
